11e3c94b19c1262dcc2a62018d061624273aaf74
Ajay <ajay.anthony@aspiresys.com>
2019-11-07 17:07:34 +0530

    Add project files.

---
+++ StudentApplication.DAL/StudentDataService.cs
@@ -0,0 +1,44 @@
+﻿using StudentApplication.Model;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace StudentApplication.DAL
+{
+    public class StudentDataService
+    {
+        SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["DatabaseConnection"].ConnectionString);
+
+        public object GetStudentData(string studentID)
+        {
+            SqlDataReader sqlDataReader = null;
+
+            StudentModel studentModel = new StudentModel();
+            using (SqlCommand sqlCommand = new SqlCommand("Select * from Student_Table where StudentID " + studentID + "", sqlConnection))
+            {
+                sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    studentModel = new StudentModel
+                    {
+                        studentID = sqlDataReader["StudentID"].ToString(),
+                        studentName = sqlDataReader["StudentName"].ToString(),
+                        studentEmail = sqlDataReader["Email"].ToString(),
+                        studentCity = sqlDataReader["City"].ToString(),
+                        dateofBirth = sqlDataReader["DateofBirth"].ToString(),
+                        studentGender = sqlDataReader["Gender"].ToString(),
+                        Department =
+                        {
+                            departmentId = sqlDataReader["DepartmentId"].ToString()
+                        }
+                    };
+
+                }
+            }
+            return studentModel;
+        }
+
+
+
+    }
+}
---
+++ StudentApplication.Model/DepartmentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication.Model
+{
+    public class DepartmentModel
+    {
+        public string departmentId { get; set; }
+
+        public string departmentName { get; set; }
+
+        public string departmentHead { get; set; }
+    }
+
+}
