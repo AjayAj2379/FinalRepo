5942cfec544fb281e35cafe68b53d7aa3a0c514e
poornima.ravichand <poornima.ravichand@ASPIRE1325>
2019-11-07 19:18:54 +0530

    model update

---
+++ StudentApplication.Model/CourseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication.Model
+{
+    public class CourseModel
+    {
+        public string courseId { get; set; }
+        public string courseName { get; set; }
+        public string lecturerId { get; set; }
+        public string departmentId { get; set; }
+        public string semesterId { get; set; }
+    }
+}
---
+++ StudentApplication.Model/SemesterModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication.Model
+{
+    public class SemesterModel
+    {
+        public string semesterId { get; set; }
+        public string semesterNumber { get; set; }
+        public string semesterMontYear { get; set; }
+    }
+}
