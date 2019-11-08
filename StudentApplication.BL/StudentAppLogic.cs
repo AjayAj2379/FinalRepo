3ee8874250a04d396fa1bbf261b93b94e50b0915
Ajay <ajay.anthony@aspiresys.com>
2019-11-08 11:21:50 +0530

    DAL updated

--- StudentApplication.BL/StudentAppLogic.cs
+++ StudentApplication.BL/StudentAppLogic.cs
@@ -31,7 +31,7 @@ namespace StudentApplication.BL
                     departName = department.departmentName
                 }
                 );
-            
+
             return result;
         }
     }
