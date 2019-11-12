7a09b116a96ddadf37f904289584bdd3026aafa2
Priyadharshini Balamurugan <priya.balamurugan@aspiresys.com>
2019-11-12 13:58:02 +0530

    Changes

--- StudentApplication.Model/ViewModels/GradeVM.cs
+++ StudentApplication.Model/ViewModels/GradeVM.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace StudentApplication.Models
+﻿namespace StudentApplication.Models
 {
     public class GradeVM
     {
